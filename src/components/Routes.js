48eed7dbce80c7a560c48f1e6bcbccc59f7ffe4f
Naman Joshi <naman.joshi@cognizant.com>
2021-06-20 13:46:23 +0530

    Webpack_Skeleton

---
+++ src/components/Routes.js
@@ -0,0 +1,10 @@
+export const components =  [{
+    "component": "Home",
+    "route": "/"
+ },{
+    "component": "DynamicPage",
+    "route": "/dynamic"
+ },{
+    "component": "NoMatch",
+    "route": "*"
+ }]
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'development',  
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[fullhash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: true,
+              },
+            },
+          },
+          {
+            loader: 'css-loader',
+             options: {
+              esModule: true,
+              modules: {
+                namedExport: true,
+              },
+            },
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true,
+    hot: true
+  }
+};
